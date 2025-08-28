6e22d8c5906b82b2a552beb40a233ec33d03ddcd
Miguel <141921930+mgznn@users.noreply.github.com>
2025-08-28 16:08:49 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,16 +4,9 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
 
-    const pessoasMundo = (dados.total_pessoas_mundo / 1e9).toFixed(1);
-    const pessoasEmpregadas = (dados.total_pessoas_empregadas / 1e9)
-    const taxaDesemprego = (dados.taxa_de_desemprego * 100)
-    const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
-    const minutos = Math.round((dados.tempo_medio_trabalho_por_semana - horas) * 60)
-
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasEmpregadas}</span> bilhões estão
-    empregadas, sendo a taxa de desemprego de <span>${taxaDesemprego}</span> %. Por fim, por semana, trabalham em média <span>${horas}</span> horas e <span>${minutos}</span> minutos.`
+    paragrafo.innerHTML = `Você sabia que o flamengo tem <span>${dados.Flamengo}</span> milhões de torcedores, o corinthians <span>${dados.Corinthians}</span> milhões, o Palmeiras <span>${dados.Palmeiras}</span> , o são paulo <span>${dados.São_Paulo}</span> ,o Gremio <span>${dados.Grêmio}</span>.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
