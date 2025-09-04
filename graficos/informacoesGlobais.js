f2c0758ee4eb54c66e936ab87f83cd9833d5852b
Miguel <141921930+mgznn@users.noreply.github.com>
2025-09-04 15:12:06 -0300

    miguel

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o flamengo tem <span>${dados.Flamengo}</span> milhões de torcedores, o corinthians <span>${dados.Corinthians}</span> milhões, o Palmeiras <span>${dados.Palmeiras}</span> , o são paulo <span>${dados.São_Paulo}</span> ,o Gremio <span>${dados.Grêmio}</span>.`
+    paragrafo.innerHTML = `Você sabia que o flamengo tem <span>${dados.Flamengo}</span> milhões de torcedores, o corinthians <span>${dados.Corinthians}</span> milhões, o Palmeiras <span>${dados.Palmeiras}</span> , o são paulo <span>${dados.Sao_Paulo}</span> ,o Gremio <span>${dados.Gremio}</span>.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
